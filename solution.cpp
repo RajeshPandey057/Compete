0939ea2ec3555d8cc94f27632e718bb7c69b4f3d
Rajesh Pandey <rajesh-pandey@outlook.in>
2023-12-30 14:15:33 +0530

    my brothaaaaa

--- solution.cpp
+++ solution.cpp
@@ -1,13 +1,42 @@
 
+/*
+     ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
+     उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
+
+     天上天下唯我獨尊
+     Throughout heaven and earth, I alone am the honored one
+
+     The bells of the Gion monastery in India echo with the warning that all things are impermenant. 
+     The blossoms of the sala trees teach us through their hues that what flourishes must fade. 
+     However! We are the exception!
+
+     Nah, I'd win. 
+ */
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
+#define int long long int
+#define double long double
+#define F first
+#define S second
+#define pb push_back
+#define bug(...) __f(#__VA_ARGS__,__VA_ARGS__)
+
+void solve(){
+
+}
+
+int32_t main(int32_t argc, char const *argv[])
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    int n;
-    cin >> n;
-    cout << n;
-    return 0;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        clock_t start = clock();
+        int t = 1;
+        cin >> t;
+        while (t--)
+            solve();
+        cerr << "Time: " << ((double)(clock() - start) / CLOCKS_PER_SEC);
+        return 0;
 }
