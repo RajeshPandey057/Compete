338b04b3347a0ba8d4a81ecb8646e2a2fb01fef5
Raj <rajesh-pandey@outlook.in>
2025-07-20 21:13:23 +0530

    chore(vscode): ✨ update VSCode configurations and add new chatmode
    * Updated `c_cpp_properties.json` to use GCC 15.
    * Modified `launch.json` to improve debugging configuration.
    * Changed `tasks.json` to reflect the new compiler path.
    * Added new chatmode file `4.1-Beast.chatmode.md` for enhanced coding assistance.

--- solution.cpp
+++ solution.cpp
@@ -6,77 +6,81 @@
      天上天下唯我獨尊
      Throughout heaven and earth, I alone am the honored one
 
-     The bells of the Gion monastery in India echo with the warning that all things are impermenant. 
-     The blossoms of the sala trees teach us through their hues that what flourishes must fade. 
+     The bells of the Gion monastery in India echo with the warning that all things are impermenant.
+     The blossoms of the sala trees teach us through their hues that what flourishes must fade.
      However! We are the exception!
 
-     Nah, I'd win. 
+     Nah, I'd win.
  */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define int            long long int
-#define F              first
-#define S              second
-#define pb             push_back
-#define si             set <int>
-#define vi             vector <int>
-#define pii            pair <int, int>
-#define vpi            vector <pii>
-#define vpp            vector <pair<int, pii>>
-#define mii            map <int, int>
-#define mpi            map <pii, int>
-#define spi            set <pii>
-#define endl           "\n"
-#define sz(x)          ((int) x.size())
-#define all(p)         p.begin(), p.end()
-#define double         long double
-#define que_max        priority_queue <int>
-#define que_min        priority_queue <int, vi, greater<int>>
-#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
-#define print(a)       for(auto x : a) cout << x << " "; cout << endl
-#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
-#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
-inline int power(int a, int b)
-{
-	int x = 1;
-	while (b)
-	{
-		if (b & 1) x *= a;
-		a *= a;
-		b >>= 1;
-	}
-	return x;
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define si set<int>
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpi vector<pii>
+#define vpp vector<pair<int, pii>>
+#define mii map<int, int>
+#define mpi map<pii, int>
+#define spi set<pii>
+#define endl "\n"
+#define sz(x) ((int)x.size())
+#define all(p) p.begin(), p.end()
+#define double long double
+#define que_max priority_queue<int>
+#define que_min priority_queue<int, vi, greater<int>>
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define print(a)                       \
+    for (auto x : a) cout << x << " "; \
+    cout << endl
+#define print1(a) \
+    for (auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a, x, y)                              \
+    for (int i = x; i < y; i++) cout << a[i] << " "; \
+    cout << endl
+inline int power(int a, int b) {
+    int x = 1;
+    while (b) {
+        if (b & 1) x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
 }
 
 template <typename Arg1>
-void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
 template <typename Arg1, typename... Args>
-void __f (const char* names, Arg1&& arg1, Args&&... args)
-{
-	const char* comma = strchr (names + 1, ',');
-	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
 }
 
 const int N = 200005;
 
-
-void solve(){
-
+void solve() {
 }
 
-int32_t main(int32_t argc, char const *argv[])
-{
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-        clock_t start = clock();
-        int t = 1;
-        cin >> t;
-        while (t--)
-            solve();
-        cerr << "Time: " << ((double)(clock() - start) / CLOCKS_PER_SEC);
-        return 0;
+int32_t main(int32_t argc, char const* argv[]) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    clock_t start = clock();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    cerr << "Hello World!" << endl;
+    cerr << "Time: " << ((double)(clock() - start) / CLOCKS_PER_SEC);
+    return 0;
 }
