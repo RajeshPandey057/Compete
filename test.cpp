3cf2ab0006e01dfc603702012d27e66e8fe90a43
Raj <rajesh-pandey@outlook.in>
2025-07-21 15:38:35 +0530

    feat: ✨ add initial implementation of `test.cpp` with basic I/O operations
    * Introduced a simple program that prints "Hello, World!" to the console.
    * Added variable manipulations to demonstrate basic arithmetic operations.

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    int x = 5;
+    int y = x - 2;
+    cout << y;
+    int z = y - x;
+    cout << z;
+    return 0;
+}
